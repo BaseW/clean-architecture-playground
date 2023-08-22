88d5e24f1b94c6b5d48d9842a8236dca77206f9d
BaseW <52400336+BaseW@users.noreply.github.com>
2023-08-22 14:24:51 +0900

    fix: fix infrastructure (#26)

--- infrastructure/src/todo_repository.rs
+++ infrastructure/src/todo_repository.rs
@@ -22,7 +22,11 @@ impl TodoRepository for SqliteTodoRepository {
             Err(e) => return Err(DomainError::Infrastructure(e.into())),
         };
         InternalSqliteTodoRepository::create(todo, &mut tx).await?;
-        Ok(())
+        let result = tx.commit().await;
+        match result {
+            Ok(_) => Ok(()),
+            Err(e) => Err(DomainError::Infrastructure(e.into())),
+        }
     }
 
     async fn find_all(&self) -> Result<Vec<Todo>, DomainError> {
@@ -50,7 +54,11 @@ impl TodoRepository for SqliteTodoRepository {
             Err(e) => return Err(DomainError::Infrastructure(e.into())),
         };
         InternalSqliteTodoRepository::update(todo, &mut tx).await?;
-        Ok(())
+        let result = tx.commit().await;
+        match result {
+            Ok(_) => Ok(()),
+            Err(e) => Err(DomainError::Infrastructure(e.into())),
+        }
     }
 
     async fn delete(&self, todo_id: i64) -> Result<(), DomainError> {
@@ -60,7 +68,11 @@ impl TodoRepository for SqliteTodoRepository {
             Err(e) => return Err(DomainError::Infrastructure(e.into())),
         };
         InternalSqliteTodoRepository::delete(todo_id, &mut tx).await?;
-        Ok(())
+        let result = tx.commit().await;
+        match result {
+            Ok(_) => Ok(()),
+            Err(e) => Err(DomainError::Infrastructure(e.into())),
+        }
     }
 }
 
@@ -78,6 +90,7 @@ impl InternalSqliteTodoRepository {
         .bind(todo.title.as_ref().unwrap_or(&"".to_string()))
         .execute(&mut *conn)
         .await;
+        println!("todo: {:?}", todo);
         match todo {
             Ok(_) => Ok(()),
             Err(e) => Err(DomainError::Infrastructure(e.into())),
@@ -134,6 +147,7 @@ impl InternalSqliteTodoRepository {
         .bind(todo.id)
         .execute(&mut *conn)
         .await;
+        println!("todo: {:?}", todo);
         match todo {
             Ok(_) => Ok(()),
             Err(e) => Err(DomainError::Infrastructure(e.into())),
@@ -154,5 +168,326 @@ impl InternalSqliteTodoRepository {
             Ok(_) => Ok(()),
             Err(e) => Err(DomainError::Infrastructure(e.into())),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use domain::entity::todo::Todo;
+    use sqlx::sqlite::SqlitePoolOptions;
+
+    async fn prepare_table(conn: &mut SqliteConnection) {
+        sqlx::query(
+            r#"
+            CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL
+            )
+            "#,
+        )
+        .execute(&mut *conn)
+        .await
+        .unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_internal() {
+        let pool = SqlitePoolOptions::new()
+            .connect("sqlite::memory:")
+            .await
+            .unwrap();
+        let mut conn = pool.acquire().await.unwrap();
+
+        prepare_table(&mut conn).await;
+
+        let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+        match todos {
+            Ok(todos) => {
+                assert_eq!(todos.len(), 0);
+            }
+            Err(_) => panic!("failed to fetch todos"),
+        };
+
+        let todo = Todo {
+            id: 0,
+            title: Some("task1".to_string()),
+        };
+        let result = InternalSqliteTodoRepository::create(&todo, &mut conn).await;
+        let todo = match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 1);
+                        let todo = todos[0].clone();
+                        assert_eq!(todo.title, Some("task1".to_string()));
+                        todo
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to create todo"),
+        };
+
+        let result = InternalSqliteTodoRepository::find_by_id(todo.id, &mut conn).await;
+        match result {
+            Ok(todo) => match todo {
+                Some(todo) => {
+                    assert_eq!(todo.title, Some("task1".to_string()));
+                }
+                None => panic!("failed to fetch todo"),
+            },
+            Err(_) => panic!("failed to fetch todo"),
+        };
+
+        let result = InternalSqliteTodoRepository::update(&todo, &mut conn).await;
+        match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 1);
+                        let todo = todos[0].clone();
+                        assert_eq!(todo.title, Some("task1".to_string()));
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to update todo"),
+        };
+
+        let result = InternalSqliteTodoRepository::delete(todo.id, &mut conn).await;
+        match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 0);
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to delete todo"),
+        };
+    }
+
+    #[tokio::test]
+    async fn test_create() {
+        let pool = SqlitePoolOptions::new()
+            .connect("sqlite::memory:")
+            .await
+            .unwrap();
+        let mut conn = pool.acquire().await.unwrap();
+
+        prepare_table(&mut conn).await;
+
+        let repository = SqliteTodoRepository::new(pool);
+        let todo = Todo {
+            id: 0,
+            title: Some("task1".to_string()),
+        };
+        let result = repository.create(&todo).await;
+        match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 1);
+                        let todo = todos[0].clone();
+                        assert_eq!(todo.title, Some("task1".to_string()));
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to create todo"),
+        };
+    }
+
+    #[tokio::test]
+    async fn test_update() {
+        let pool = SqlitePoolOptions::new()
+            .connect("sqlite::memory:")
+            .await
+            .unwrap();
+        let mut conn = pool.acquire().await.unwrap();
+
+        prepare_table(&mut conn).await;
+
+        let repository = SqliteTodoRepository::new(pool);
+        let todo = Todo {
+            id: 0,
+            title: Some("task1".to_string()),
+        };
+        let result = repository.create(&todo).await;
+        let todo = match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 1);
+                        let todo = todos[0].clone();
+                        assert_eq!(todo.title, Some("task1".to_string()));
+                        todo
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to create todo"),
+        };
+
+        let todo = Todo {
+            id: todo.id,
+            title: Some("task2".to_string()),
+        };
+        let result = repository.update(&todo).await;
+        match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 1);
+                        let todo = todos[0].clone();
+                        assert_eq!(todo.title, Some("task2".to_string()));
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to update todo"),
+        };
+    }
+
+    #[tokio::test]
+    async fn test_delete() {
+        let pool = SqlitePoolOptions::new()
+            .connect("sqlite::memory:")
+            .await
+            .unwrap();
+        let mut conn = pool.acquire().await.unwrap();
+
+        prepare_table(&mut conn).await;
+
+        let repository = SqliteTodoRepository::new(pool);
+        let todo = Todo {
+            id: 0,
+            title: Some("task1".to_string()),
+        };
+        let result = repository.create(&todo).await;
+        let todo = match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 1);
+                        let todo = todos[0].clone();
+                        assert_eq!(todo.title, Some("task1".to_string()));
+                        todo
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to create todo"),
+        };
+
+        let result = repository.delete(todo.id).await;
+        match result {
+            Ok(_) => {
+                let todos = InternalSqliteTodoRepository::find_all(&mut conn).await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 0);
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to delete todo"),
+        };
+    }
+
+    #[tokio::test]
+    async fn test_find_all() {
+        let pool = SqlitePoolOptions::new()
+            .connect("sqlite::memory:")
+            .await
+            .unwrap();
+        let mut conn = pool.acquire().await.unwrap();
+
+        prepare_table(&mut conn).await;
+
+        let repository = SqliteTodoRepository::new(pool);
+
+        let todos = repository.find_all().await;
+        match todos {
+            Ok(todos) => {
+                assert_eq!(todos.len(), 0);
+            }
+            Err(_) => panic!("failed to fetch todos"),
+        };
+
+        let todo = Todo {
+            id: 0,
+            title: Some("task1".to_string()),
+        };
+        let result = InternalSqliteTodoRepository::create(&todo, &mut conn).await;
+        match result {
+            Ok(_) => {
+                let todos = repository.find_all().await;
+                match todos {
+                    Ok(todos) => {
+                        assert_eq!(todos.len(), 1);
+                        let todo = todos[0].clone();
+                        assert_eq!(todo.title, Some("task1".to_string()));
+                        todo
+                    }
+                    Err(_) => panic!("failed to fetch todos"),
+                }
+            }
+            Err(_) => panic!("failed to create todo"),
+        };
+    }
+
+    #[tokio::test]
+    async fn test_find_by_id() {
+        let pool = SqlitePoolOptions::new()
+            .connect("sqlite::memory:")
+            .await
+            .unwrap();
+        let mut conn = pool.acquire().await.unwrap();
+
+        prepare_table(&mut conn).await;
+
+        let repository = SqliteTodoRepository::new(pool);
+
+        let todo = repository.find_by_id(1).await;
+        match todo {
+            Ok(todo) => {
+                if todo.is_some() {
+                    panic!("todo should not be found");
+                }
+            }
+            Err(_) => panic!("failed to fetch todo"),
+        };
+
+        let todo = Todo {
+            id: 0,
+            title: Some("task1".to_string()),
+        };
+        let result = InternalSqliteTodoRepository::create(&todo, &mut conn).await;
+        match result {
+            Ok(_) => {
+                let todo = repository.find_by_id(1).await;
+                match todo {
+                    Ok(todo) => match todo {
+                        Some(todo) => {
+                            assert_eq!(todo.title, Some("task1".to_string()));
+                            todo
+                        }
+                        None => panic!("todo should be found"),
+                    },
+                    Err(_) => panic!("failed to fetch todo"),
+                }
+            }
+            Err(_) => panic!("failed to create todo"),
+        };
     }
 }
