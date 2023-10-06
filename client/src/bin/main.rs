8bb5ff737363f73f0381a6b3bc14c9978a1d09f3
BaseW <52400336+BaseW@users.noreply.github.com>
2023-10-06 22:13:39 +0900

    feat: add package for rest client (#31)

---
+++ client/src/bin/main.rs
@@ -0,0 +1,64 @@
+use client::rest::{create_todo, delete_todo, find_todo, get_todos, update_todo};
+
+#[tokio::main]
+async fn main() {
+    // parse args
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: client <command>");
+        return;
+    }
+
+    // command: get_todos, find_todo, create_todo, update_todo, delete_todo
+    let command = &args[1];
+    match command.as_str() {
+        "help" => {
+            println!("Usage: client <command>");
+            println!("Commands:");
+            println!("  get_todos");
+            println!("  find_todo <id>");
+            println!("  create_todo <title>");
+            println!("  update_todo <id> <title>");
+            println!("  delete_todo <id>");
+        }
+        "get_todos" => {
+            get_todos().await.unwrap();
+        }
+        "find_todo" => {
+            if args.len() < 3 {
+                println!("Usage: client find_todo <id>");
+                return;
+            }
+            let id = args[2].parse::<i64>().unwrap();
+            find_todo(id).await.unwrap();
+        }
+        "create_todo" => {
+            if args.len() < 3 {
+                println!("Usage: client create_todo <title>");
+                return;
+            }
+            let title = args[2].clone();
+            create_todo(title).await.unwrap();
+        }
+        "update_todo" => {
+            if args.len() < 4 {
+                println!("Usage: client update_todo <id> <title>");
+                return;
+            }
+            let id = args[2].parse::<i64>().unwrap();
+            let title = args[3].clone();
+            update_todo(id, title).await.unwrap();
+        }
+        "delete_todo" => {
+            if args.len() < 3 {
+                println!("Usage: client delete_todo <id>");
+                return;
+            }
+            let id = args[2].parse::<i64>().unwrap();
+            delete_todo(id).await.unwrap();
+        }
+        _ => {
+            println!("Usage: client <command>");
+        }
+    }
+}
---
+++ client/src/rest.rs
@@ -0,0 +1,68 @@
+use reqwest::Client;
+
+pub async fn get_todos() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let res = client.get("http://localhost:8080/todos").send().await?;
+    println!("Status: {}", res.status());
+    let body = res.text().await?;
+    println!("Body: {}", body);
+    Ok(())
+}
+
+pub async fn find_todo(id: i64) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let res = client
+        .get(format!("http://localhost:8080/todos/{}", id))
+        .send()
+        .await?;
+    println!("Status: {}", res.status());
+    let body = res.text().await?;
+    println!("Body: {}", body);
+    Ok(())
+}
+
+pub async fn create_todo(title: String) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let res = client
+        .post("http://localhost:8080/todos")
+        .json(&serde_json::json!({
+            "title": title,
+        }))
+        .send()
+        .await?;
+    println!("Status: {}", res.status());
+    let body = res.text().await?;
+    println!("Body: {}", body);
+    Ok(())
+}
+
+pub async fn update_todo(id: i64, title: String) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let res = client
+        .put("http://localhost:8080/todos")
+        .json(&serde_json::json!({
+            "id": id,
+            "title": title,
+        }))
+        .send()
+        .await?;
+    println!("Status: {}", res.status());
+    let body = res.text().await?;
+    println!("Body: {}", body);
+    Ok(())
+}
+
+pub async fn delete_todo(id: i64) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let res = client
+        .delete("http://localhost:8080/todos")
+        .json(&serde_json::json!({
+            "id": id,
+        }))
+        .send()
+        .await?;
+    println!("Status: {}", res.status());
+    let body = res.text().await?;
+    println!("Body: {}", body);
+    Ok(())
+}
